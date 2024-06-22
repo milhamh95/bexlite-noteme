5cbe531a3956b6b04ce09e413d23df22c08362d8
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2024-06-22 19:28:39 +0700

    feat: crud notes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { client } from "./models/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/notes", () => {
+    const allNotes = client.query("SELECT * FROM notes").all();
+
+    return {data: allNotes}
+  })
+  .post("/notes", ({body}) => {
+    const {content} = body;
+    client.query("INSERT INTO notes (content) VALUES (?)").run(content);
+
+    return {message: "success"};
+  }, {
+    body: t.Object({
+      content: t.String(),
+    })
+  })
+  .delete("/notes/:id", ({params}) => {
+    client.query("DELETE FROM notes WHERE id = ?").run(params.id);
+
+    return {
+      message: `Notes has been deleted`
+    };
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
